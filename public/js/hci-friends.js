4eb77826a81b648a9f83afd8c0ccf175d92e5a34
Kyly <kylyvass@gmail.com>
2016-02-07 21:35:02 -0800

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -14,10 +14,10 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
@@ -44,3 +44,11 @@ function anagrammedName(name) {
 		return name;
 	}
 }
+
+$(".names").click(function(){
+	console.log("working");
+	var name = $(this).text();
+	console.log("name")
+	name = anagrammedName(name);
+	$(this).text(name);
+})
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+   res.render('add', data);
+   var fName = req.query.name;
+   var fDesp = req.query.description;
+   var imageLink = "http://lorempixel.com/400/400/people/"
+   var jsonStr = {
+	   'name': fName,
+	   'description': fDesp,
+	   'imageURL': imageLink
+   };
+
+   console.log(jsonStr);
+   data["friends"].push(jsonStr);
+}
